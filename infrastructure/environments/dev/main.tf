ad356e4612707d2357ac45c7137bd3185fb8d377
Shayan Sadeghieh <shayan.sadeghieh@gmail.com>
2024-08-31 18:25:45 -0400

    init the backend providers

--- infrastructure/environments/dev/main.tf
+++ infrastructure/environments/dev/main.tf
@@ -16,18 +16,3 @@ resource "google_storage_bucket" "default" {
   }
 }
 
-resource "local_file" "default" {
-  file_permission = "0644"
-  filename        = "${path.module}/backend.tf"
-
-  # You can store the template in a file and use the templatefile function for
-  # more modularity, if you prefer, instead of storing the template inline as
-  # we do here.
-  content = <<-EOT
-  terraform {
-    backend "gcs" {
-      bucket = "${google_storage_bucket.default.name}"
-    }
-  }
-  EOT
-}
